03ce2fb03f422ff3818b472fc9c9a80cef48e5a9
Anseryde <ryanlupis@gmail.com>
2025-05-25 00:05:56 +0800

    testing dotfiles home manager module

---
+++ modules/homemanager/misc_dotfiles_src/misc_dotfiles_src.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, config, ...  }: # lib exposes useful opts like lib.mkIf, config lets you manipulate config in this file
+{
+  options = {
+    exemplar_misc_dotfiles_src.enable =
+  	  lib.mkEnableOption "enables misc_dotfiles_src";
+  };
+  config = lib.mkIf config.misc_dotfiles_src.enable {
+    xdg.configFile."OpenTabletDriver" = {
+      source = ./OpenTabletDriver;
+      recursive = true;
+    };
+  };
+}
