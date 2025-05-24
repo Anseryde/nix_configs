c27d6fee7cd2f4c0c912741ccc4bf9305f3c2740
Anseryde <ryanlupis@gmail.com>
2025-05-24 16:43:18 +0800

    enabled udev service

--- hosts/oldfart12/configuration.nix
+++ hosts/oldfart12/configuration.nix
@@ -221,6 +221,9 @@
   # Waydroid
   virtualisation.waydroid.enable = true;
 
+  # udev
+  services.udev.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- hosts/vyasapura/configuration.nix
+++ hosts/vyasapura/configuration.nix
@@ -219,6 +219,9 @@
   # Waydroid
   virtualisation.waydroid.enable = true;
 
+  # udev
+  services.udev.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
