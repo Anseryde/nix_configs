5410c2af0449d1ced55f7c2bad3059045bcb0ea5
Anseryde <ryanlupis@gmail.com>
2025-05-25 16:18:38 +0800

    configured helix as a home manager module

---
+++ modules/homemanager/terminal/helix.nix
@@ -0,0 +1,21 @@
+
+{ pkgs, lib, config, ...  }: # lib exposes useful opts like lib.mkIf, config lets you manipulate config in this file
+{
+  options = {
+    helix.enable =
+  	  lib.mkEnableOption "enables helix";
+  };
+  config = lib.mkIf config.helix.enable {
+    programs.helix = {
+      enable = true;
+      settings = {
+        theme = "dracula_at_night";
+        editor.cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+    };
+  };
+}
