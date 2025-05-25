61470aaef2a099f33ce9faf803b4ce03a4555619
Anseryde <ryanlupis@gmail.com>
2025-05-25 16:19:29 +0800

    added helix to system packages

--- modules/nixos/pkgs.nix
+++ modules/nixos/pkgs.nix
@@ -10,6 +10,7 @@
     environment.systemPackages = with pkgs; [
       #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       micro
+      helix
       wget
       distrobox
       epson-escpr2
