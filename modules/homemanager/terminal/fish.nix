3b856f2cce116ea508010f2b5bc09355c3cc62a2
Anseryde <ryanlupis@gmail.com>
2025-05-26 20:32:38 +0800

    modularised fish home-manager module and added starship prompt, should further modularise starship if I want to customise it (forgot to run git add .)

---
+++ modules/homemanager/terminal/fish.nix
@@ -0,0 +1,25 @@
+
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+# lib exposes useful opts like lib.mkIf, config lets you manipulate config in this file
+{
+  options = {
+    fish.enable =
+      lib.mkEnableOption "enables fish";
+  };
+  config = lib.mkIf config.fish.enable {
+    programs.fish = {
+      enable = true;
+      shellInit = "
+        starship init fish | source
+      ";
+    };
+    home.packages = with pkgs; [
+      starship
+    ];
+  };
+}
