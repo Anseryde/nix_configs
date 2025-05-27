63a22510c0a1f658bd6459284797de9c93affcf3
Anseryde <ryanlupis@gmail.com>
2025-05-27 21:33:52 +0800

    modularised yazi

---
+++ modules/homemanager/terminal/yazi.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+# lib exposes useful opts like lib.mkIf, config lets you manipulate config in this file
+{
+  options = {
+    yazi.enable =
+      lib.mkEnableOption "enables yazi";
+  };
+  config = lib.mkIf config.yazi.enable {
+    programs.yazi = {
+      enable = true;
+      enableFishIntegration = true;
+      plugins = {
+        git = pkgs.yaziPlugins.git;
+        sudo = pkgs.yaziPlugins.sudo;
+        chmod = pkgs.yaziPlugins.chmod;
+        duckdb = pkgs.yaziPlugins.duckdb;
+        full-border = pkgs.yaziPlugins.full-border;
+        glow = pkgs.yaziPlugins.glow;
+        restore = pkgs.yaziPlugins.restore;
+        mediainfo = pkgs.yaziPlugins.mediainfo;
+      };
+    };
+  };
+}
