ed686c1d9f6de94182bde18765caf0c8858eec31
Anseryde <ryanlupis@gmail.com>
2025-05-28 19:28:12 +0800

    switched out docker for podman and modularised users

---
+++ modules/nixos/users.nix
@@ -0,0 +1,37 @@
+
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+# lib exposes useful opts like lib.mkIf, config lets you manipulate config in this file
+{
+  options = {
+    users.enable =
+      lib.mkEnableOption "enables users";
+  };
+  config = lib.mkIf config.users.enable {
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.ryann = {
+      isNormalUser = true;
+      extraGroups = ["wheel" "networkmanager" "docker"]; # Enable ‘sudo’ for the user.
+      shell = pkgs.fish;
+      packages = with pkgs; [
+        tree
+      ];
+      subGidRanges = [
+        {
+          count = 65536;
+          startGid = 1000;
+        }
+      ];
+      subUidRanges = [
+        {
+          count = 65536;
+          startUid = 1000;
+        }
+      ];
+    };
+  };
+}
