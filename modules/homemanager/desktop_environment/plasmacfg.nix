8bd4d1192b7d1346f4f75879d227afefc8a9b4f2
Anseryde <ryanlupis@gmail.com>
2025-05-29 23:22:45 +0800

    pushed weather widget further to the right

--- modules/homemanager/desktop_environment/plasmacfg.nix
+++ modules/homemanager/desktop_environment/plasmacfg.nix
@@ -35,7 +35,7 @@
         {
           name = "org.kde.plasma.weather";
           position = {
-            horizontal = 900;
+            horizontal = 1000;
             vertical = 20;
           };
           size = {
