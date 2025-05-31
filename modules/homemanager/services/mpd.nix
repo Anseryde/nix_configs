7244ef4ff35736109caec676b2d7c73ba16801d3
Anseryde <ryanlupis@gmail.com>
2025-05-31 22:44:41 +0800

    set mpd music directory to absolute path /home/ryann/Music

--- modules/homemanager/services/mpd.nix
+++ modules/homemanager/services/mpd.nix
@@ -13,6 +13,7 @@
   config = lib.mkIf config.mpd.enable {
     services.mpd = {
       enable = true;
+      musicDirectory = /home/ryann/Music;
       network = {
         startWhenNeeded = true;
         listenAddress = "any";
