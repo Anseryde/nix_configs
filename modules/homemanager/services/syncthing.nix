adacea37577100fdf6c112b559ccc4dc81e7ed66
Anseryde <ryanlupis@gmail.com>
2025-06-02 21:56:12 +0800

    updated syncthing module with declarations of folders shared

--- modules/homemanager/services/syncthing.nix
+++ modules/homemanager/services/syncthing.nix
@@ -13,7 +13,37 @@
   config = lib.mkIf config.syncthing.enable {
     services.syncthing = {
       enable = true;
+      dataDir = "/home/ryann";
+      configDir = "/home/ryann/.config/syncthing";
       settings = {
+        devices = {
+          "SM-G781B" = { id = "5HJT7HM-EM5YKX2-WODZXEQ-23XQWVY-2X2LDOF-4VDZYDC-QFUWKJH-3JISXAR"; };
+        };
+        folders = {
+          "Synced Music" = {
+            id = "edide-wfgxv";
+            path = "/home/ryann/Music";
+            devices = [ "SM-G781B" ];
+          };
+          "Obsidian" = {
+            id = "ory6u-xp2zs";
+            path = "/home/ryann/Documents/Obsidian";
+            devices = [ "SM-G781B" ];
+          };
+          "Calibre Library" = {
+            id = "qgpzw-gplks";
+            path = "/home/ryann/Documents/Calibre Library";
+            devices = [ "SM-G781B" ];
+          };
+          "Portable Tablet Art" = {
+            id = "2lznr-iaa66";
+            path = "/home/ryann/Documents/Art/Krita/Portable Tablet Art";
+            devices = [ "SM-G781B" ];
+          };
+        };
+        gui = {
+          user = "ryanlupis";
+        };
         options = {
           listenAddresses = [
             "default"
