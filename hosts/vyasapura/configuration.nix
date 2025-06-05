2f342cc56566c22d27cf95a5e0de3fc54b07d24b
Anseryde <ryanlupis@gmail.com>
2025-06-05 14:44:52 +0800

    declaratively set boot loader timeout to be 10 seconds for vyasapura

--- hosts/vyasapura/configuration.nix
+++ hosts/vyasapura/configuration.nix
@@ -24,6 +24,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 10;
 
   #switch to latest stable linux kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
