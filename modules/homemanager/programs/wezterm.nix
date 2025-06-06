a1bb039101e3c089ad21af27c106416ae696911c
Anseryde <ryanlupis@gmail.com>
2025-06-06 17:50:24 +0800

    commented out wezterm background

--- modules/homemanager/programs/wezterm.nix
+++ modules/homemanager/programs/wezterm.nix
@@ -22,17 +22,17 @@
         config.font_size = 12.0
         config.enable_wayland = false
         config.enable_scroll_bar = true
-        config.window_background_image = '/home/ryann/Pictures/NitW Backer Files/Wallpapers/NITW_Backer_Wallpapers.zip/Desktop/Emoticons/NITW_Emoticons_wide_2560x1440.png'
-        config.window_background_image_hsb = {
-          brightness = 0.1,
+        -- config.window_background_image = '/home/ryann/Pictures/NitW Backer Files/Wallpapers/NITW_Backer_Wallpapers.zip/Desktop/Emoticons/NITW_Emoticons_wide_2560x1440.png'
+        -- config.window_background_image_hsb = {
+        --   brightness = 0.1,
 
-          -- You can adjust the hue by scaling its value.
-          -- a multiplier of 1.0 leaves the value unchanged.
-          hue = 1.0,
+        --   -- You can adjust the hue by scaling its value.
+        --   -- a multiplier of 1.0 leaves the value unchanged.
+        --   hue = 1.0,
 
-          -- You can adjust the saturation also.
-          saturation = 1.0,
-        }
+        --   -- You can adjust the saturation also.
+        --   saturation = 1.0,
+        -- }
 
         return config
       '';
