ed021deb1e52294f863082c99037673e98abd78f
Anseryde <ryanlupis@gmail.com>
2025-07-10 14:26:37 +0800

    added armorpaint compilation dev shell

---
+++ nixdevshells/armortools shells/armorpaint compile/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = with pkgs; [
+        clang
+        xorg.libXinerama
+        xorg.libXrandr
+        alsa-lib
+        xorg.libXi
+        mesa
+        libGL
+        xorg.libXcursor
+        vulkan-tools
+        gtk3
+        udev
+      ];
+    };
+  };
+}
