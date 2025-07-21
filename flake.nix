6942316845186978cacc47856003acb66ac7c7cd
Anseryde <ryanlupis@gmail.com>
2025-07-21 21:57:23 +0800

    switched back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 # flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
