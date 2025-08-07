0491b7e9f24c6b04abf9488b3327dbd8cd686cfc
Anseryde <ryanlupis@gmail.com>
2025-08-07 11:56:35 +0800

    explicitly make firefox use firefox-bin instead of building from source

--- modules/homemanager/programs/firefox.nix
+++ modules/homemanager/programs/firefox.nix
@@ -13,9 +13,7 @@
   config = lib.mkIf config.firefox.enable {
     programs.firefox = {
       enable = true;
-      package = with pkgs; [
-        firefox-bin
-      ];
+      package = pkgs.firefox-bin; 
       languagePacks = ["en-US" "en-GB" "zh-CN" "zh-TW"];
       policies = {
         DisableTelemetry = true;
