35b37d2b9d8a761206aaeeb187dc243482086ad9
Anseryde <ryanlupis@gmail.com>
2025-08-10 15:43:06 +0800

    undid prior

--- modules/nixos/programs/nvf-config.nix
+++ modules/nixos/programs/nvf-config.nix
@@ -21,10 +21,6 @@
           };
           dashboard = {
             enabled = true;
-            sections = ''
-              { section = "header" },
-              { section = "keys", gap = 1, padding = 1 },
-            '';
           };
           explorer = {
             enabled = true;
