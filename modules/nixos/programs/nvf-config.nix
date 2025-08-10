1be573d64320396113532778d09bd8f4bb42b9ae
Anseryde <ryanlupis@gmail.com>
2025-08-10 23:30:13 +0800

    attempting to add custom plugin from github

--- modules/nixos/programs/nvf-config.nix
+++ modules/nixos/programs/nvf-config.nix
@@ -148,6 +148,23 @@
         };
       };
     };
+    startPlugins = [
+      {
+        pname = "nvim-toggler";
+        version = "0.3.1";
+        src = pkgs.fetchFromGithub {
+          owner = "nguyenvukhang";
+          repo = "nvim-toggler";
+          ref = "origin/main";
+          hash = "467808600882fd6c9e33b9dbc4889b1b80cfd917";
+        };
+        # Whether to place plugin in /start or /setupOpt 
+        optional = false;
+        # Plugins can have other plugins as dependencies
+        # this is mainly used in nixpkgs, avoid it if possible
+        dependencies = [];
+      }
+    ];
     extraPlugins = with pkgs.vimPlugins; {
       nightfox-nvim = {
         package = nightfox-nvim;
