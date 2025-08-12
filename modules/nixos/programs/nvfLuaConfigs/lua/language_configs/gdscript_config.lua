dbd36e4ce539976ac73506e857e7f3c228d6ff85
Anseryde <ryanlupis@gmail.com>
2025-08-12 23:47:25 +0800

    attempting further gdscript lsp config

--- modules/nixos/programs/nvfLuaConfigs/lua/language_configs/gdscript_config.lua
+++ modules/nixos/programs/nvfLuaConfigs/lua/language_configs/gdscript_config.lua
@@ -1,3 +1,12 @@
 -- gdscript.lua
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-require('lspconfig').gdscript.setup(capabilities)
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- require('lspconfig').gdscript.setup(capabilities)
+require('lspconfig').gdscript.setup({
+        name = "gdscript";
+        -- Fill in your Godot Language Server Parameters
+        cmd = vim.lsp.rpc.connect('127.0.0.1', 6005),
+        -- Fill in where Neovim listens to Godot LSP
+        on_init = function (client, init_result)
+                vim.fn.serverstart './godothost'
+        end
+})
