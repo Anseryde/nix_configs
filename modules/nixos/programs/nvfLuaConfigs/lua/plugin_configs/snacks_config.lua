d4d64018d2d447b668598a34d6a3a6df3dade3aa
Anseryde <ryanlupis@gmail.com>
2025-08-13 11:31:54 +0800

    Enabled snacks.nvim image module

--- modules/nixos/programs/nvfLuaConfigs/lua/plugin_configs/snacks_config.lua
+++ modules/nixos/programs/nvfLuaConfigs/lua/plugin_configs/snacks_config.lua
@@ -1,48 +1,51 @@
 -- snacks.lua
 require("snacks").setup({
-        dashboard = {
-                enabled = true,
-                -- sections = {
-                --   { section = "header" },
-                --   { section = "keys", gap = 1, padding = 1 },
-                -- },
-                sections = {
-                        { section = "header" },
-                        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-                        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                        --[[       { section = "startup" }, ]]
-                },
-        },
-        bigfile = {
-                enabled = true,
-        },
-        explorer = {
-                enabled = true,
-        },
-        indent = {
-                enabled = true,
-        },
-        notifier = {
-                enabled = true,
-                timeout = 3000,
-        },
-        picker = {
-                enabled = true,
-        },
-        quickfile = {
-                enabled = true,
-        },
-        scope = {
-                enabled = true,
-        },
-        scroll = {
-                enabled = true,
-        },
-        statuscolumn = {
-                enabled = true,
-        },
-        words = {
-                enabled = true,
-        },
+	dashboard = {
+		enabled = true,
+		-- sections = {
+		--   { section = "header" },
+		--   { section = "keys", gap = 1, padding = 1 },
+		-- },
+		sections = {
+			{ section = "header" },
+			{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+			{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+			{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+			--[[       { section = "startup" }, ]]
+		},
+	},
+	bigfile = {
+		enabled = true,
+	},
+	explorer = {
+		enabled = true,
+	},
+	indent = {
+		enabled = true,
+	},
+	notifier = {
+		enabled = true,
+		timeout = 3000,
+	},
+	picker = {
+		enabled = true,
+	},
+	quickfile = {
+		enabled = true,
+	},
+	scope = {
+		enabled = true,
+	},
+	scroll = {
+		enabled = true,
+	},
+	statuscolumn = {
+		enabled = true,
+	},
+	words = {
+		enabled = true,
+	},
+	image = {
+		enabled = true,
+	},
 })
