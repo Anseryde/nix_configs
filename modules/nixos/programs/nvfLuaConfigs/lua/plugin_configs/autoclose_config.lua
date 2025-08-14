2f604ccc8332fc3e571f43d29f5d004fc3372824
Anseryde <ryanlupis@gmail.com>
2025-08-14 10:49:07 +0800

    Disabled pairing for ' character in markdown files in neovim in autoclose_config.lua

--- modules/nixos/programs/nvfLuaConfigs/lua/plugin_configs/autoclose_config.lua
+++ modules/nixos/programs/nvfLuaConfigs/lua/plugin_configs/autoclose_config.lua
@@ -1,6 +1,11 @@
 -- autoclose-nvim config
 require("autoclose").setup({
+	keys = {
+		["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "markdown" } },
+	},
 	options = {
-		disabled_filetypes = { "text"--[[ , "markdown" ]] },
+		disabled_filetypes = {
+			"text" --[[ , "markdown" ]],
+		},
 	},
 })
