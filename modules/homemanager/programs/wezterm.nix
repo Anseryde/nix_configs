94fd52481eb5e001f147797476dbff28325dfc28
Anseryde <ryanlupis@gmail.com>
2025-11-11 12:09:10 +0800

    Enabled wayland for wezterm

--- modules/homemanager/programs/wezterm.nix
+++ modules/homemanager/programs/wezterm.nix
@@ -20,7 +20,7 @@
         config.color_scheme = 'Ibm3270 (Gogh)'
         config.font = wezterm.font 'Courier Prime'
         config.font_size = 12.0
-        config.enable_wayland = false
+        config.enable_wayland = true
         config.scrollback_lines = 10000
         config.enable_scroll_bar = true
         -- config.window_background_image = '/home/ryann/Pictures/NitW Backer Files/Wallpapers/NITW_Backer_Wallpapers.zip/Desktop/Emoticons/NITW_Emoticons_wide_2560x1440.png'
