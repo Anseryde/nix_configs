ce9ec48f48adb53d64a7ba8eb22ec305a45859e4
Anseryde <ryanlupis@gmail.com>
2025-11-17 22:39:28 +0800

    Switched from nouveau NVK to nvidia proprietary again. Might try nouveau and NVK again when mesa 23.6 and kernel 6.19 hit, could get DLSS and better performance…

--- hosts/vyasapura/configuration.nix
+++ hosts/vyasapura/configuration.nix
@@ -14,9 +14,9 @@
   ];
   # custom module selection
   # for full list of modules, as well as default modules, view nix-configs/modules/nixos/default.nix
-  nvidia-open-kernel-module.enable = false;
-  nvidia-optimus.enable = false;
-  opensourcenvidia.enable = true;
+  nvidia-open-kernel-module.enable = true;
+  nvidia-optimus.enable = true;
+  opensourcenvidia.enable = false;
   steam.enable = true;
   gamescopeandgamemode.enable = true;
   msi-ec.enable = true;
