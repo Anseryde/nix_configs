d46041042e025be63e22bcf4abfdd0fe8bfb726f
Anseryde <ryanlupis@gmail.com>
2025-12-09 16:02:25 +0800

    switched firewall backend to firewalld

--- hosts/vyasapura/configuration.nix
+++ hosts/vyasapura/configuration.nix
@@ -227,7 +227,13 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
+    backend = "firewalld";
+  };
+  services.firewalld = {
+    enable = true;
+  };
 
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
